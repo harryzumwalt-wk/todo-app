015bd9b021db0f9f44ee129002eeddc409766713
harryzumwalt-wk <harry.zumwalt@workiva.com>
2023-12-13 17:31:11 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [text, setText] = useState(''); 
+  const [items, setItems] = useState([]); 
+
+  function handleClick(e) {
+    setItems([...items, text])
+  }
+
+  function handleChange(e) {
+    setText(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <div className="todo-app">
+            <h2>To-Do List <img src="/images/icon.png" /></h2>
+            <div className="row">
+                <input type="text" id="input-box" placeholder="Add your text" value={text} onChange={handleChange} />
+                <button onClick={handleClick}>Add</button>
+            </div>
+            <ul id="list-container">
+              {items.map(function(todo) {
+                return <li>{todo}<><span>{"\u00D7"}</span></></li>
+              })}
+            </ul>
+        </div>
     </div>
   );
 }
